601d98258e07c33a0ed5f5b485f32a9f6cba6fcd
Hamzah Iqbal <hamzahiqbal@Hamzahs-MacBook-Air.local>
2025-06-15 19:15:38 +0100

    second page overlaps front page

--- src/components/cards/project/projectCard.tsx
+++ src/components/cards/project/projectCard.tsx
@@ -34,65 +34,71 @@ function ProjectCard() {
 
   // TODO: both functions and divs, can be refactored
 
+  const SliderFrontPage = () => {
+    return (
+      <>
+        <motion.div
+          className="w-full h-[50%] bg-amber-600 z-20 absolute border-black border-2 pointer-events-none"
+          animate={
+            clicked ? {
+              y: `0`,
+            } : {
+              y: `var(--desktop-header-height)`
+            }
+          }
+          initial={false}
+          transition={{ type: 'tween', ease: 'easeInOut' }}
+        >
+          <div className='pointer-events-auto'>
+
+            <Button className='bg-red-900' onClick={() => handleClick(!clicked)}>Toggle Expand {JSON.stringify(clicked)}</Button>
+          </div>
+        </motion.div>
+
+        <motion.div
+          className='w-40 h-40 z-30 absolute bg-red-600 
+    top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
+          animate={{
+            scale: clicked ? 1.5 : 1,
+          }}
+          initial={false}
+          transition={{
+            type: "spring",
+            stiffness: 80,
+            damping: 20,
+            mass: 0.7,
+          }}
+        />
+
+        <motion.div
+          className="w-full h-[50%] top-1/2 bg-amber-600 z-20 absolute border-black border-2 pointer-events-none"
+          initial={false}
+
+          animate={
+            clicked ? {
+              y: `0`,
+            } : {
+              y: `calc(-1 * var(--desktop-footer-height))`
+            }
+          }
+          transition={{ type: 'tween', ease: 'easeInOut' }}
+        >
+        </motion.div>
+      </>
+    )
+  }
 
   return (
     <>
-      <motion.div
-        className="w-full h-[50%] bg-amber-600 z-20 absolute border-black border-2 pointer-events-none"
-        animate={
-          clicked ? {
-            y: `0`,
-          } : {
-            y: `var(--desktop-header-height)`
-          }
-        }
-        transition={{ type: 'tween', ease: 'easeInOut' }}
-      >
-        <div className='pointer-events-auto'>
-
-          <Button className='bg-red-900' onClick={() => handleClick(!clicked)}>Toggle Expand {JSON.stringify(clicked)}</Button>
-        </div>
-      </motion.div>
-
-      <motion.div
-        className='w-40 h-40 z-30 absolute bg-red-600 
-    top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
-        animate={{
-          scale: clicked ? 1.5 : 1,
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 80,
-          damping: 20,
-          mass: 0.7,
-        }}
-      />
-
-      <motion.div
-        className="w-full h-[50%] top-1/2 bg-amber-600 z-20 absolute border-black border-2 pointer-events-none"
-        animate={
-          clicked ? {
-            y: `0`,
-          } : {
-            y: `calc(-1 * var(--desktop-footer-height))`
-          }
-        }
-        transition={{ type: 'tween', ease: 'easeInOut' }}
-      >
-      </motion.div>
+      {SliderFrontPage()}
 
       <div
         ref={scrollRef}
         className="relative flex-1 overflow-auto bg-amber-300">
         {/* Amber full background */}
-        <motion.div
-          className="absolute inset-0 bg-amber-600 -z-10"
-        >
-          {/* You can put content here if needed */}
-        </motion.div>
 
         {/* Orange scrollable content with transparency */}
-        <motion.div className="relative bg-amber-400 bg-opacity-60 h-[200vh] p-4 z-10 text-black">
+        <motion.div className="bg-amber-400 bg-opacity-60 translate-y-full absolute h-full w-full p-4 z-999 text-black">
           Orange scrolls over amber with transparency.<br />
           Scroll down to see the effect.
         </motion.div>

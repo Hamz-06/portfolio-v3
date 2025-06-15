70cb16dfaa59f4e91d318b7d0b794ae4c221b1b9
Hamzah Iqbal <hamzahiqbal@Hamzahs-MacBook-Air.local>
2025-06-15 10:35:31 +0100

    added image

--- src/components/cards/project/projectCard.tsx
+++ src/components/cards/project/projectCard.tsx
@@ -25,17 +25,16 @@ function ProjectCard() {
     );
     return footerHeight;
   }
-
+  // TODO: both functions and divs, can be refactored
   return (
     <div className="flex-1 bg-amber-300 relative">
       {/* top half for transition */}
       <motion.div
-        className="w-full h-[50%] bg-amber-600 z-999 absolute border-black border-2"
+        className="w-full h-[50%] bg-amber-600 z-20 absolute border-black border-2"
         animate={
           clicked ? {
             y: `-${getHeaderHeight()}px`,
           } : {
-            padding: '20px',
             y: 0
           }
         }
@@ -44,10 +43,24 @@ function ProjectCard() {
         <Button className='bg-red-900' onClick={() => handleClick(!clicked)}>Toggle Expand {JSON.stringify(clicked)}</Button>
       </motion.div>
 
+      <motion.div
+        className='w-40 h-40 z-30 absolute bg-red-600 
+    top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
+        animate={{
+          scale: clicked ? 1.5 : 1,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 80,
+          damping: 20,
+          mass: 0.7,
+        }}
+      />
+
 
       {/*  bottom half have to add this for smooth transition */}
       <motion.div
-        className="w-full h-[50%] top-1/2 bg-amber-600 z-999 absolute border-black border-2"
+        className="w-full h-[50%] top-1/2 bg-amber-600 z-20 absolute border-black border-2"
         animate={
           clicked ? {
             y: `+${getFooterHeight()}px`,
@@ -60,7 +73,12 @@ function ProjectCard() {
       >
         <Button className='bg-red-900' onClick={() => handleClick(!clicked)}>Toggle Expand {JSON.stringify(clicked)}</Button>
       </motion.div>
-    </div>
+
+      {/* create paralax effect on this div
+      <motion.div className='w-56 h-56  z-999 absolute top-full bg-pink-600'>
+        lolllll
+      </motion.div> */}
+    </div >
   )
 }
 
